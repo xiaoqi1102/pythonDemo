a7b62e8dff020ef196589676bcf5ba487a00757c
小齐 <705764065@qq.com>
2017-06-08 07:53:39 +0800

    test pillow module

--- module.py
+++ module.py
@@ -1,3 +1,9 @@
-import partial
 
-print partial.test()
+from PIL import Image
+
+im = Image.open('./img/test.png')
+
+print(im.format,im.size)
+
+im.thumbnail((200,100))
+im.save('thumb.jpg','JPEG')
---
+++ oop.py
@@ -0,0 +1,10 @@
+class Student(object):
+    def __init__(self,name,score):
+        self.name=name
+        self.score=score
+    def print_score(self):
+        print('%s:%s' % (self.name,self.score))
+
+
+xiaoqi=Student('xiaoqi',100)
+xiaoqi.print_score()
